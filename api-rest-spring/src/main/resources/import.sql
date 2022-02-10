f56e0910c395e17c7520a6d97cd33229cb4b35d4
MissStrange <esanchezloarte@gmail.com>
2022-02-10 11:25:02 +0100

    primer commit

---
+++ api-rest-spring/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO clientes (nombre, apellido, email, telefono) VALUES ("Jose", "Perez", "jp@gmail.com", 678345123);
+INSERT INTO clientes (nombre, apellido, email, telefono) VALUES ("Luis", "Ruiz", "lr@gmail.com", 698674532);
+INSERT INTO clientes (nombre, apellido, email, telefono) VALUES ("María", "Muñoz", "mm@gmail.com", 645321897);
+INSERT INTO clientes (nombre, apellido, email, telefono) VALUES ("Jonathan", "Sánchez", "js@gmail.com", 678652173);
+INSERT INTO clientes (nombre, apellido, email, telefono) VALUES ("Mateo", "Lopez", "ml@gmail.com", 623478904);
